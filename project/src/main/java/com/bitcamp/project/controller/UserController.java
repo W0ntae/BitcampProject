0af2671d60dcac7810d157262ee751ce0017727e
w0ntae <bitcampcoding@gmail.com>
2022-10-11 16:53:39 +0900

    .

--- project/src/main/java/com/bitcamp/project/controller/UserController.java
+++ project/src/main/java/com/bitcamp/project/controller/UserController.java
@@ -6,7 +6,10 @@ import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
@@ -19,6 +22,12 @@ public class UserController {
   public String ViewUsers(Model model){
     List<User> users = userService.findAll();
     model.addAttribute("users", users);
+    return "User/AdminUser";
+  }
+  
+  @PostMapping("/viewUsers")
+  public String deletedUser(Model model, HttpServletRequest request, HttpServletResponse response){
+    
     return "User/AdminUser";
   }
 }
--- project/src/main/java/com/bitcamp/project/mapper/UserMapper.java
+++ project/src/main/java/com/bitcamp/project/mapper/UserMapper.java
@@ -9,4 +9,6 @@ import java.util.List;
 @Mapper
 public interface UserMapper {
   List<User> findUsers();
+  
+  void deleteUser(@Param("id")int id);
 }
